8e89e511f33e9f3e36eb37b982e7b65f762a1c74
alikafel12 <alikafel12@gmail.com>
2019-09-23 18:33:46 -0400

    Changing PokeDex UI

--- pokedex.py
+++ pokedex.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'Pokedex.ui'
-#
-# Created by: PyQt5 UI code generator 5.6
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sqlite3
 
